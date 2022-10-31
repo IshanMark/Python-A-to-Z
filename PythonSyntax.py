bd819ff549a05b49ea79f319b281bab025dbcc08
IshanMark <rupasing-cs18036@stu.kln.ac.lk>
2022-10-31 21:37:40 +0530

    add python syntax

---
+++ PythonSyntax.py
@@ -0,0 +1,22 @@
+# Execute Python Syntax
+    # Write Directly in CMD
+
+print("Hello Imashi")
+
+# Pyhton Indentation
+    #Spaces at the beginning of a code line
+
+if 9>5:
+    print("9 is greater than 5")
+
+#if 9>5: Get Error no Space
+#print("9 is greater than 5")
+
+#Python Variables
+    # python variables are created when you assign a valur to it
+    # No command for declaring a variable.
+    # Case Sensitive
+x0 = 1 # Int
+x1 = 1.5 # Float
+y = "Gamage" # String
+
