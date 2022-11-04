16c808227175d5e1cb1c7db93785e3426e42b94f
IshanMark <rupasing-cs18036@stu.kln.ac.lk>
2022-11-04 22:42:12 +0530

    Basic String

---
+++ String.py
@@ -0,0 +1,44 @@
+#String
+
+print("Mark")
+print('Mark')
+
+print()
+#Assign String to Variable
+a= "Mark"
+print(a)
+
+#Multiline String
+b="""I am Mark, I am Undergraduage ,
+I want best coder in srilanka"""
+
+print(b)
+
+#String are Arrays
+c = "Hello Anjana"
+print(a[1])
+
+print()
+#Looping Through a String
+for d in "Mark":
+    print(d)
+
+print()
+#String Length
+e = "Hello Imashi"
+print(len(e))
+
+print()
+#Check String
+f = "I crush is Imashi Anjana Gamage"
+print("crush" in f)
+print()
+if "crush" in f:
+    print("Yes,'crush' word have")
+
+print()
+print("love" not in f)
+print()
+if "love" not in f:
+    print("Nom 'love' word not have")
+
