fee51dd50b32afa8f2666b8a95aa244ff715a93c
IshanMark <rupasing-cs18036@stu.kln.ac.lk>
2022-11-04 20:57:17 +0530

    Add Casting

---
+++ Casting.py
@@ -0,0 +1,28 @@
+# Cating
+x = int(1)
+y = int(2.8)
+z = int("3")
+
+print(x)
+print(y)
+print(z)
+
+print()
+a = float(1)
+b = float(2.8)
+c = float("3")
+d = float("4.2")
+
+print(a)
+print(b)
+print(c)
+print(d)
+
+print()
+e = str("s1")
+f = str(2)
+z = str(3.4)
+
+print(e)
+print(f)
+print(z)
